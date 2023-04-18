1c3f5e4a31cd1c5c667b2cc910206912f65225e8
coder <soumyodipsutra@gmail.com>
2023-04-18 23:15:15 +0530

    initial commit

--- app/src/main/java/com/example/groceryapp/GroceryRVAdapter.kt
+++ app/src/main/java/com/example/groceryapp/GroceryRVAdapter.kt
@@ -1,4 +1,41 @@
 package com.example.groceryapp
 
-class GroceryRVAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class GroceryRVAdapter(
+    var list: List<GroceryItems>,
+    val groceryItemClickInterface: GroceryItemClickInterface
+    ): RecyclerView.Adapter<GroceryRVAdapter.GroceryViewHolder>() {
+
+
+
+    inner class GroceryViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val nameTV=itemView.findViewById<TextView>(R.id.idTVItemName)
+        val quantityTV=itemView.findViewById<TextView>(R.id.idTVQuantity)
+        val rateTV=itemView.findViewById<TextView>(R.id.idTVRate)
+        val amountTV=itemView.findViewById<TextView>(R.id.idTVTotalAmt)
+        val deleteTV=itemView.findViewById<TextView>(R.id.idIVDelete)
+
+    }
+
+    interface GroceryItemClickInterface{
+        fun onItemClick(groceryItems: GroceryItems){
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroceryViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: GroceryViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
