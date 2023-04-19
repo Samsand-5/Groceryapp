5d8f477365871f9fef49776169e13380d35cfc47
coder <soumyodipsutra@gmail.com>
2023-04-19 21:44:59 +0530

    initial commit

--- app/src/main/java/com/example/groceryapp/MainActivity.kt
+++ app/src/main/java/com/example/groceryapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.groceryapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -13,32 +16,60 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class MainActivity : AppCompatActivity(),GroceryRVAdapter.GroceryItemClickInterface {
     lateinit var itemsRV: RecyclerView
     lateinit var addFAB: FloatingActionButton
-    lateinit var list:List<GroceryItems>
-    lateinit var groceryRVAdapter:GroceryRVAdapter
+    lateinit var list: List<GroceryItems>
+    lateinit var groceryRVAdapter: GroceryRVAdapter
     lateinit var groceryViewModal: GroceryViewModal
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        itemsRV=findViewById(R.id.idRVItems)
-        addFAB=findViewById(R.id.idFABAdd)
+        itemsRV = findViewById(R.id.idRVItems)
+        addFAB = findViewById(R.id.idFABAdd)
         list = ArrayList<GroceryItems>()
-        groceryRVAdapter = GroceryRVAdapter(list,this)
+        groceryRVAdapter = GroceryRVAdapter(list, this)
         itemsRV.layoutManager = LinearLayoutManager(this)
         itemsRV.adapter = groceryRVAdapter
         val groceryRepository = GroceryRepository(GroceryDatabase(this))
         val factory = GroceryViewModelFactory(groceryRepository)
-        groceryViewModal = ViewModelProvider(this,factory).get(GroceryViewModal::class.java)
-        groceryViewModal.getAllGroceryItems().observe(this,{
+        groceryViewModal = ViewModelProvider(this, factory).get(GroceryViewModal::class.java)
+        groceryViewModal.getAllGroceryItems().observe(this, {
             groceryRVAdapter.list = it
             groceryRVAdapter.notifyDataSetChanged()
         })
 
-        addFAB.setOnClickListener{
-
+        addFAB.setOnClickListener {
+            openDialog()
         }
-        fun openDialog(){
+    }
 
+    fun openDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.grocery_add_dialog)
+        val cancelBtn = dialog.findViewById<Button>(R.id.idBtnCancel)
+        val addBtn = dialog.findViewById<Button>(R.id.idBtnAdd)
+        val itemEdt = dialog.findViewById<EditText>(R.id.idEdtItemName)
+        val itemPriceEdt = dialog.findViewById<EditText>(R.id.idEdtItemPrice)
+        val itemQuantityEdt = dialog.findViewById<EditText>(R.id.idEdtItemQuantity)
+        cancelBtn.setOnClickListener {
+            dialog.dismiss()
         }
+        addBtn.setOnClickListener{
+            val itemName : String = itemEdt.text.toString()
+            val itemQuantity : String = itemQuantityEdt.text.toString()
+            val itemPrice : String = itemPriceEdt.text.toString()
+            val qty : Int = itemQuantity.toInt()
+            val pr : Int = itemPrice.toInt()
+            if(itemName.isNotEmpty()&&itemQuantity.isNotEmpty()&&itemPrice.isNotEmpty()){
+                val items = GroceryItems(itemName,qty,pr)
+                groceryViewModal.insert(items)
+                Toast.makeText(applicationContext,"Item inserted",Toast.LENGTH_SHORT).show()
+                groceryRVAdapter.notifyDataSetChanged()
+                dialog.dismiss()
+            } else{
+                Toast.makeText(applicationContext,"Please Enter all the Data",Toast.LENGTH_SHORT).show()
+
+            }
+        }
+        dialog.show()
     }
 
     override fun onItemClick(groceryItems: GroceryItems) {
